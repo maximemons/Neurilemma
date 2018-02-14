96a6973c2e5b9f5e52283fc9a2cb3af2bed6c543
Maxime Mons <maxime.mons@etudiant.univ-lille1.fr>
2018-02-14 11:54:12 +0100

    Debut web

--- webserver/main.c
+++ webserver/main.c
@@ -9,7 +9,8 @@
 
 
 /*-----------*/
-#define SIZE_BUF 	4096
+#define DEF_PORT 8080
+#define SIZE_BUF 4096
 
 /*-----------*/
 
@@ -30,19 +31,26 @@ void initialiser_signaux(void){
 }
 
 
-int main(/*int argc, char** argv*/){
+int main(int argc, char** argv){
 	int socket_serveur, socket_client;
 	const char* message_bienvenue = "\n\n\e[2m*******************************************************\n	neurilemma - noun [nu̇r-ə-ˈle-mə] : the plasma\n  membrane surrounding a Schwann cell of a myelinated\n  nerve fiber and separating layers of myelin\n*******************************************************\e[22m\n    \e[1m\e[4m\"Neurilemma ? Ne vous prenez pas la tête !\"\e[21m\e[24m\n\n\n    \e[1m=> Soyez les bienvenus sur Neurilemma ! <=\e[21m\n\n\n  Neurilemma est un serveur utilisé par les plus \ngrandes entreprises du monde tel que \e[1mGoogle\e[21m, \e[1mFacebook\e[21m,\n\e[1mTwitter\e[21m, \e[1mAmazon\e[21m, et maintenant \e[1mvous\e[21m :D\n\n  La force de ce projet est sa simplicité et son\nefficacité. En effet, Neurilimma à été pensé pour vous\nsimplifier la vie, par deux talentueux étudiants \e[1mMelvin\nBELEMBERT\e[21m et \e[1mMaxime MONS\e[21m, qui eux, pour le coup, se sont\nbien compliqués la vie pour le réaliser.\n\n\n\t\t\t\t\t\e[7mVersion 1.1 beta\e[27m\n\n";
 	
 	initialiser_signaux();
 
-	socket_serveur = creer_serveur(8080);
-	
+	if(argc > 1){
+		socket_serveur = creer_serveur(atoi(argv[1]));
+		printf("Sever running on port %d\n", atoi(argv[1]));
+	}
+	else{
+		socket_serveur = creer_serveur(DEF_PORT);
+		printf("Sever running on port %d\n", DEF_PORT);
+	}fflush(stdout);
+
 	while(1){
 		socket_client = accept (socket_serveur, NULL, NULL);
 
 		if(socket_client == -1){
-			perror ("accept");
+			perror("accept");
 			return -1;
 		}
 
@@ -51,10 +59,10 @@ int main(/*int argc, char** argv*/){
 			write(socket_client, message_bienvenue, strlen(message_bienvenue));
 			char buf[SIZE_BUF];
 
-
+			FILE *client = fdopen(socket_client, "w+");
 			while(1){
 				bzero(buf,SIZE_BUF);
-				if(read(socket_client, buf, SIZE_BUF) <= 0){
+				/*if(read(socket_client, buf, SIZE_BUF) <= 0){
 					perror("read");
 					break;
 				}else{
@@ -62,7 +70,10 @@ int main(/*int argc, char** argv*/){
 						perror("write");
 						break;
 					}
-				}
+				}*/
+				if(fgets(buf, SIZE_BUF, client) == NULL)
+					break;
+				if(fprintf(client))
 			}
 			close(socket_client);	
 			return 0;
