11739a96060f5812d9f8b75944a29b827710e6c7
Maxime Mons <maxime.mons@etudiant.univ-lille1.fr>
2018-02-15 08:52:34 +0100

    reponses.txt

--- webserver/main.c
+++ webserver/main.c
@@ -33,7 +33,7 @@ void initialiser_signaux(void){
 
 int main(int argc, char** argv){
 	int socket_serveur, socket_client;
-	const char* message_bienvenue = "\n\n\e[2m*******************************************************\n	neurilemma - noun [nu̇r-ə-ˈle-mə] : the plasma\n  membrane surrounding a Schwann cell of a myelinated\n  nerve fiber and separating layers of myelin\n*******************************************************\e[22m\n    \e[1m\e[4m\"Neurilemma ? Ne vous prenez pas la tête !\"\e[21m\e[24m\n\n\n    \e[1m=> Soyez les bienvenus sur Neurilemma ! <=\e[21m\n\n\n  Neurilemma est un serveur utilisé par les plus \ngrandes entreprises du monde tel que \e[1mGoogle\e[21m, \e[1mFacebook\e[21m,\n\e[1mTwitter\e[21m, \e[1mAmazon\e[21m, et maintenant \e[1mvous\e[21m :D\n\n  La force de ce projet est sa simplicité et son\nefficacité. En effet, Neurilimma à été pensé pour vous\nsimplifier la vie, par deux talentueux étudiants \e[1mMelvin\nBELEMBERT\e[21m et \e[1mMaxime MONS\e[21m, qui eux, pour le coup, se sont\nbien compliqués la vie pour le réaliser.\n\n\n\t\t\t\t\t\e[7mVersion 1.1 beta\e[27m\n\n\n> ";
+	//const char* message_bienvenue = "\n\n\e[2m*******************************************************\n	neurilemma - noun [nu̇r-ə-ˈle-mə] : the plasma\n  membrane surrounding a Schwann cell of a myelinated\n  nerve fiber and separating layers of myelin\n*******************************************************\e[22m\n    \e[1m\e[4m\"Neurilemma ? Ne vous prenez pas la tête !\"\e[21m\e[24m\n\n\n    \e[1m=> Soyez les bienvenus sur Neurilemma ! <=\e[21m\n\n\n  Neurilemma est un serveur utilisé par les plus \ngrandes entreprises du monde tel que \e[1mGoogle\e[21m, \e[1mFacebook\e[21m,\n\e[1mTwitter\e[21m, \e[1mAmazon\e[21m, et maintenant \e[1mvous\e[21m :D\n\n  La force de ce projet est sa simplicité et son\nefficacité. En effet, Neurilimma à été pensé pour vous\nsimplifier la vie, par deux talentueux étudiants \e[1mMelvin\nBELEMBERT\e[21m et \e[1mMaxime MONS\e[21m, qui eux, pour le coup, se sont\nbien compliqués la vie pour le réaliser.\n\n\n\t\t\t\t\t\e[7mVersion 1.1 beta\e[27m\n\n\n> ";
 	
 	initialiser_signaux();
 
@@ -56,24 +56,15 @@ int main(int argc, char** argv){
 
 		if(fork() == 0){
 			close(socket_serveur);
-			write(socket_client, message_bienvenue, strlen(message_bienvenue));
+			//write(socket_client, message_bienvenue, strlen(message_bienvenue));
 			char buf[SIZE_BUF];
 
 			FILE *client = fdopen(socket_client, "w+");
 			while(1){
 				bzero(buf,SIZE_BUF);
-				/*if(read(socket_client, buf, SIZE_BUF) <= 0){
-					perror("read");
-					break;
-				}else{
-					if(write(socket_client, buf, strlen(buf)) <= 0){
-						perror("write");
-						break;
-					}
-				}*/
 				if(fgets(buf, SIZE_BUF, client) == NULL) break;
-				if(fprintf(client, "\033[1m<Neurilemma>:\033[21m %s", buf) < 0) break;
-				if(fprintf(client, "> ") < 0) break;
+				//if(fprintf(client, "\033[1m<Neurilemma>:\033[21m %s\n> ", buf) < 0) break;
+				printf("%s\n", buf);
 			}
 			close(socket_client);	
 			return 0;
