a26d5b80c366f5648cde6f25feac47bdb234a031
monsm <maxime.mons@etudiant.univ-lille1.fr>
2018-02-22 14:49:27 +0100

    Get v2

--- webserver/main.c
+++ webserver/main.c
@@ -28,8 +28,9 @@ void initialiser_signaux(void){
 	if(sigaction(SIGCHLD,&sa ,NULL) == -1)
 		perror("sigaction (SIGCHLD)");
 }
-void badRequest(FILE *client){
-	fprintf(client, "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad Request\r\n");
+void badRequest(FILE *client, int code){
+	if(code == 400)fprintf(client, "HTTP/1.1 400 Bad Request\r\nConnection: close\r\nContent-Length: 17\r\n\r\n400 Bad Request\r\n");
+	if(code == 404)fprintf(client, "HTTP/1.1 400 Not Found\r\nConnection: close\r\nContent-Length: 15\r\n\r\n404 Not Found\r\n");
 	exit(0);
 }
 void goodRequest(FILE *client, const char* msg){
@@ -39,7 +40,7 @@ void goodRequest(FILE *client, const char* msg){
 int main(int argc, char** argv){
 	int socket_serveur, socket_client;
 	//const char* message_bienvenue = "\n\n\e[2m*******************************************************\n	neurilemma - noun [nu̇r-ə-ˈle-mə] : the plasma\n  membrane surrounding a Schwann cell of a myelinated\n  nerve fiber and separating layers of myelin\n*******************************************************\e[22m\n    \e[1m\e[4m\"Neurilemma ? Ne vous prenez pas la tête !\"\e[21m\e[24m\n\n\n    \e[1m=> Soyez les bienvenus sur Neurilemma ! <=\e[21m\n\n\n  Neurilemma est un serveur utilisé par les plus \ngrandes entreprises du monde tel que \e[1mGoogle\e[21m, \e[1mFacebook\e[21m,\n\e[1mTwitter\e[21m, \e[1mAmazon\e[21m, et maintenant \e[1mvous\e[21m :D\n\n  La force de ce projet est sa simplicité et son\nefficacité. En effet, Neurilimma à été pensé pour vous\nsimplifier la vie, par deux talentueux étudiants \e[1mMelvin\nBELEMBERT\e[21m et \e[1mMaxime MONS\e[21m, qui eux, pour le coup, se sont\nbien compliqués la vie pour le réaliser.\n\n\n\t\t\t\t\t\e[7mVersion 1.1 beta\e[27m\n\n\n> ";
-	const char* message_bienvenue = "<center><img src=\"https://media0.giphy.com/media/BIuuwHRNKs15C/200.gif\" height=\"100%\"/></center>";
+	const char* message_bienvenue = "Hakuna Matata";
 
 	initialiser_signaux();
 
@@ -67,24 +68,48 @@ int main(int argc, char** argv){
 
 			FILE *client = fdopen(socket_client, "w+");
 			while(1){
-				int request = 0;
+				int request = 400;
 				bzero(buf,SIZE_BUF);
 				// 1er Fgets pour récuprer la premiere ligne (la ligne GET)
 				if(fgets(buf, SIZE_BUF, client) == NULL) break;
-				// Analyser la requetet ... sauvegarder le resultat dans un boolean 
-				if(strncmp(buf,"GET ",4)==0 && strncmp(buf + 4, "/ ", 2)==0 && (strncmp(buf + 6, "HTTP/1.1", 8)==0 || strncmp(buf + 6, "HTTP/1.0", 8)==0)){
-					request = 1;
-				}
+				// Analyser la requetet ... sauvegarder le resultat dans un boolean
+				char* token = strtok(buf, " ");
+				char *comp[4] = {"GET", "/", "HTTP/1.0\r\n", "HTTP/1.1\r\n"};
+				int cpt;
+				//int valide = 0;
+				for(cpt = 0; cpt < 4; cpt++){
+					printf("%s & %s => %d\n", token, comp[cpt], strcmp(token, comp[cpt]));
+					if(cpt != 2) token = strtok(NULL, " ");
+				}printf("%s & %s => %d\n", "TEST", "TEST", strcmp("TEST", "TEST"));
+				printf("%s & %s => %d\n", "HTTP/1.1", "HTTP1.1", strcmp("HTTP/1.1", "HTTP/1.1"));
+				fflush(stdout);
+
+    			/*char *token;
+    			char *comp[4] = {"GET", "/", "HTTP/1.0", "HTTP/1.1"};
+			    token = strtok(buf," ");
+			    int cpt;
+			    int check = 0;
+			    for(cpt = 0; cpt < 5; cpt++)
+			    	if(strncmp(token, comp[cpt], strlen(comp[cpt])) != 0) check++;
+			    printf("%d", check);
+			    fflush(stdout);
+			    if(check) request = 1;
+
+				if(strncmp(buf,"GET ",4)==0 && strncmp(buf + 4, "/", 1)==0 && (strncmp(buf + 6, "HTTP/1.1", 8)==0 || strncmp(buf + 6, "HTTP/1.0", 8)==0)){
+					if(strncmp(buf + 4, "/ ", 2)==0) request = 1;
+					else request = 404;
+				}*/
 				/*Skip header*/
 				while(fgets(buf, SIZE_BUF, client)){
 					if(strcmp(buf, "\r\n") == 0){
 						break;
 					}
 				}
+				request = 1;
 				// Envoyer réponse !!!
 				//fprintf(client, "%s", message_bienvenue);
 				if(request) goodRequest(client, message_bienvenue);
-				else badRequest(client);
+				else badRequest(client, request);
 
 			}
 			close(socket_client);	
