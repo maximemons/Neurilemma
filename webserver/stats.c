8ba4bea5340e7dda863caa28faedc1b773c07456
monsm <maxime.mons@etudiant.univ-lille1.fr>
2018-03-24 13:00:38 +0100

    shared memorie

--- webserver/stats.c
+++ webserver/stats.c
@@ -3,21 +3,22 @@
 #include <sys/mman.h>
 
 /*-----------*/
-static web_stats stats;
+static web_stats* stats;
 
 /*-----------*/
 int init_stats(void){
-	mmap(NULL, sizeof(stats), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-  	stats.served_connections = 0;
-  	stats.served_requests = 0;
-  	stats.ok_200 = 0;
-  	stats.ko_400 = 0;
-  	stats.ko_403 = 0;
-  	stats.ko_404 = 0;
+  stats = mmap(NULL, sizeof(stats), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+  
+  stats -> served_connections = 0;
+  stats -> served_requests = 0;
+  stats -> ok_200 = 0;
+  stats -> ko_400 = 0;
+  stats -> ko_403 = 0;
+  stats -> ko_404 = 0;
 
-  	return 1;
+  return 1;
 }
 
 web_stats *get_stats(void){
-	return &stats;
+	return stats;
 }
